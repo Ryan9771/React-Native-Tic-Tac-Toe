cd7353f6350d84cb2686e53bc7fb5ffd822185e7
ryan <ryan.patel21@imperial.ac.uk>
2022-09-09 21:46:43 +0800

    Made some changes to the minimax algo, but doesn't seem to work

--- app/config/miniMax.js
+++ app/config/miniMax.js
@@ -5,34 +5,34 @@ import { checkWin } from './gameLogicSingleplayer';
  */
 export function bestMove(board) {
     let bestScore = -Infinity;
-    let bestMove;
+    let move;
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             // It is empty
             if (board[i][j] === '-') { 
                 board[i][j] = 'O';
-                let score = minimax(board, false);
+                let score = minimax(board, false, 0);
                 board[i][j] = '-';
                 if (score > bestScore) {
                     bestScore = score;
-                    bestMove = [i, j];
+                    move = [i, j];
                 }
             }
         }
     }
-    return bestMove;
+    return move;
 }
 
 let scores = {
-    'X': 1,
-    'O': -1,
+    'X': -10,
+    'O': 10,
     'tie': 0,
 }
 
 /**
  * Algorithm to determine the optimal move for the AI to do (1 or 0)
  */
-function minimax(board, ai) {
+function minimax(board, ai, depth) {
     if (checkWin(board)[0] > 0) {
         let winner = checkWin(board)[1];
         return scores[winner];
@@ -44,7 +44,7 @@ function minimax(board, ai) {
             for (let j = 0; j < 3; j++) {
                 if (board[i][j] === '-') {
                     board[i][j] = 'O';
-                    let score = minimax(board, false);
+                    let score = minimax(board, false, depth+1);
                     board[i][j] = '-';
                     bestScore = Math.max(score, bestScore);
                 }
@@ -58,7 +58,7 @@ function minimax(board, ai) {
             for (let j = 0; j < 3; j++) {
                 if (board[i][j] === '-') {
                     board[i][j] = 'X';
-                    let score = minimax(board, true);
+                    let score = minimax(board, true, depth+1);
                     board[i][j] = '-'
                     bestScore = Math.min(score, bestScore);
                 }
